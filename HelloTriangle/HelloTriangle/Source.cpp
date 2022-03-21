22d5549515d2e4eb030ecec433425f38604b7e35
Vanessa Silveira <52571672+vnssilveira@users.noreply.github.com>
2022-03-21 17:51:19 -0300

    Ajustando texto.

--- HelloTriangle/HelloTriangle/Source.cpp
+++ HelloTriangle/HelloTriangle/Source.cpp
@@ -68,7 +68,7 @@ int main()
 //#endif
 
 	// Criação da janela GLFW
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ola Triangulo! - Vanessa Silveira", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Olá, Triângulo! - Vanessa Silveira", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	// Fazendo o registro da função de callback para a janela GLFW
